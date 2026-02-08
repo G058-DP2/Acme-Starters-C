7983c806b8fd745147fbb13951df358cac840646
sbf6606 <fraayadia@alum.us.es>
2026-02-08 18:13:23 +0100

    task 0008/D: entidad Invention añadida

---
+++ src/main/java/acme/entities/invention/Invention.java
@@ -0,0 +1,100 @@
+
+package acme.entities.invention;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Moment;
+import acme.client.components.datatypes.Money;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidUrl;
+import acme.client.helpers.SpringHelper;
+import acme.constraints.ValidHeader;
+import acme.constraints.ValidText;
+import acme.constraints.ValidTicker;
+import acme.features.inventor.part.PartRepository;
+import acme.realms.Inventor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Invention extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidTicker
+	@Column(unique = true)
+	private String				ticker;
+
+	@Mandatory
+	@ValidHeader
+	@Column
+	private String				name;
+
+	@Mandatory
+	@ValidText
+	@Column
+	private String				description;
+
+	@Mandatory
+	@ValidMoment(constraint = ValidMoment.Constraint.ENFORCE_FUTURE)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Moment				startMoment;
+
+	@Mandatory
+	@ValidMoment(constraint = ValidMoment.Constraint.ENFORCE_FUTURE)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Moment				endMoment;
+
+	@Optional
+	@ValidUrl
+	@Column
+	private String				moreInfo;
+
+	@Mandatory
+	@Valid
+	@Column
+	private Boolean				draftMode;
+
+
+	@Valid
+	@Transient
+	public Double getMonthsActive() {
+		long diffInMillis = this.endMoment.getTime() - this.startMoment.getTime();
+		double diff = diffInMillis / (1000.0 * 60.0 * 60.0 * 24.0 * 30.0);
+		return Math.round(diff * 10.0) / 10.0;
+	}
+
+	@Transient
+	public Money getCost() {
+		Money res = new Money();
+		res.setAmount(0.00);
+		res.setCurrency("EUR");
+
+		PartRepository r;
+		r = SpringHelper.getBean(PartRepository.class);
+
+		Double total = r.findPartsByInvention(this.getId()).stream().map(Part::getCost).mapToDouble(Money::getAmount).sum();
+
+		res.setAmount(total);
+		return res;
+	}
+
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private Inventor inventor;
+
+}
