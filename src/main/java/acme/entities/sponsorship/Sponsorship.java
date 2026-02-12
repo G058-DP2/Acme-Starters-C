3055bddb9ad6f32fe95c88706be42adb3fec184e
Nicolas Parrilla <nicpargen@alum.us.es>
2026-02-12 09:58:46 +0100

    feat: fix derived attributes

--- src/main/java/acme/entities/sponsorship/Sponsorship.java
+++ src/main/java/acme/entities/sponsorship/Sponsorship.java
@@ -1,7 +1,7 @@
 
 package acme.entities.sponsorship;
 
-import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,7 +12,10 @@ import javax.persistence.Transient;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Moment;
 import acme.client.components.datatypes.Money;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
@@ -23,6 +26,7 @@ import acme.client.components.validation.ValidUrl;
 import acme.constraints.ValidHeader;
 import acme.constraints.ValidText;
 import acme.constraints.ValidTicker;
+import acme.features.sponsor.sponsorship.SponsorshipRepository;
 import acme.realms.Sponsor;
 import lombok.Getter;
 import lombok.Setter;
@@ -34,29 +38,29 @@ public class Sponsorship extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
 
-	private static final long	serialVersionUID	= 1L;
+	private static final long		serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
 	@Mandatory
 	@ValidTicker
 	@Column(unique = true)
-	private String				ticker;
+	private String					ticker;
 
 	@Mandatory
 	@ValidHeader
 	@Column
-	private String				name;
+	private String					name;
 
 	@Mandatory
 	@ValidText
 	@Column
-	private String				description;
+	private String					description;
 
 	@Mandatory
 	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date				startMoment;
+	private Moment					startMoment;
 
 	/*
 	 * startMoment/endMoment must be a valid time interval in future wrt.
@@ -66,44 +70,71 @@ public class Sponsorship extends AbstractEntity {
 	@Mandatory
 	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date				endMoment;
+	private Moment					endMoment;
 
 	@Optional
 	@ValidUrl
 	@Column
-	private String				moreInfo;
+	private String					moreInfo;
 
 	@Mandatory
 	@Valid
 	@Column
 	//Sponsorships cannot be published unless they have at least one donation
-	private Boolean				draftMode;
+	private Boolean					draftMode;
 
 	// Derived attributes -----------------------------------------------------
+
+	@Transient
+	@Autowired
+	private SponsorshipRepository	repository;
 
 	@Mandatory
 	@Valid
 	@Transient
-	/*
-	 * monthsActive is computed as the number of months in interval
-	 * startMoment/endMoment rounded to the nearest decimal
-	 */
-	private Double				monthsActive;
+	private Double					monthsActive;
 
 	@Mandatory
 	@ValidMoney(min = 0.0)
 	@Transient
-	/*
-	 * The total money of a sponsorship is the sum of money in the corresponding donations.
-	 * Only Euros are accepted.
-	 */
-	private Money				totalMoney;
+	private Money					totalMoney;
+
+
+	public Double getMonthsActive() {
+
+		double result = 0.0;
+
+		if (this.startMoment != null && this.endMoment != null) {
+			long diffInMillies = Math.abs(this.endMoment.getTime() - this.startMoment.getTime());
+			long diffInDays = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+
+			double months = diffInDays / 30.0;
+			result = Math.round(months * 10.0) / 10.0;
+		}
+
+		return result;
+	}
+
+	public Money getTotalMoney() {
+
+		Double totalAmount = this.repository.calculateTotalAmountBySponsorshipId(this.getId());
+
+		if (totalAmount == null)
+			totalAmount = 0.0;
+
+		Money res = new Money();
+		res.setAmount(totalAmount);
+		res.setCurrency("EUR");
+
+		return res;
+	}
 
 	// Relationships ----------------------------------------------------------
+
 
 	@NotNull
 	@Valid
 	@ManyToOne(optional = false)
-	private Sponsor				sponsor;
+	private Sponsor sponsor;
 
 }
--- src/main/java/acme/features/sponsor/sponsorship/SponsorshipRepository.java
+++ src/main/java/acme/features/sponsor/sponsorship/SponsorshipRepository.java
@@ -1,18 +1,14 @@
 
 package acme.features.sponsor.sponsorship;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
-import acme.entities.sponsorship.Sponsorship;
 
 @Repository
 public interface SponsorshipRepository extends AbstractRepository {
 
-	// Esta consulta busca todos los Sponsorships de un Sponsor y calcula el totalMoney al vuelo
-	@Query("select s, " + "COALESCE((select sum(d.money.amount) from Donation d where d.sponsorship = s and d.money.currency = 'EUR'), 0.0) " + "from Sponsorship s where s.sponsor.id = :sponsorId")
-	Collection<Sponsorship> findSponsorshipsWithTotalMoneyBySponsorId(int sponsorId);
+	@Query("select sum(d.money.amount) from Donation d where d.sponsorship.id = :id and d.money.currency = 'EUR'")
+	Double calculateTotalAmountBySponsorshipId(int id);
 }
