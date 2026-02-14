084ea48e297f4d9ab4632d3bffb5b688a06e8dba
sbf6606 <fraayadia@alum.us.es>
2026-02-14 16:27:33 +0100

    fix: anotación añadida al repositorio de Part

--- src/main/java/acme/features/inventor/part/PartRepository.java
+++ src/main/java/acme/features/inventor/part/PartRepository.java
@@ -4,10 +4,12 @@ package acme.features.inventor.part;
 import java.util.Collection;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
 import acme.entities.invention.Part;
 
+@Repository
 public interface PartRepository extends AbstractRepository {
 
 	@Query("SELECT p FROM Part p WHERE p.invention.id = :inventionId")
