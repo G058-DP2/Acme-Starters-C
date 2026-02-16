adf6ed409787c37d718e5bcf73e4c0a7eeb1df11
sbf6606 <fraayadia@alum.us.es>
2026-02-16 18:22:04 +0100

    fix: consulta para calcular la suma de costes directamente

--- src/main/java/acme/features/inventor/part/PartRepository.java
+++ src/main/java/acme/features/inventor/part/PartRepository.java
@@ -1,18 +1,15 @@
 
 package acme.features.inventor.part;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
-import acme.entities.invention.Part;
 
 @Repository
 public interface PartRepository extends AbstractRepository {
 
-	@Query("SELECT p FROM Part p WHERE p.invention.id = :inventionId")
-	Collection<Part> findPartsByInvention(int inventionId);
+	@Query("SELECT SUM(p.cost.amount) FROM Part p WHERE p.invention.id=:inventionId")
+	Double getSumCostsByInvention(int inventionId);
 
 }
