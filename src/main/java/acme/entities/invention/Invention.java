5306f081b296d25356b8cea908799a553fcae191
sbf6606 <fraayadia@alum.us.es>
2026-02-16 13:52:59 +0100

    fix: tipo cambiado de Moment a Date

--- src/main/java/acme/entities/invention/Invention.java
+++ src/main/java/acme/entities/invention/Invention.java
@@ -1,6 +1,8 @@
 
 package acme.entities.invention;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -10,7 +12,6 @@ import javax.persistence.Transient;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
-import acme.client.components.datatypes.Moment;
 import acme.client.components.datatypes.Money;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
@@ -50,12 +51,12 @@ public class Invention extends AbstractEntity {
 	@Mandatory
 	@ValidMoment(constraint = ValidMoment.Constraint.ENFORCE_FUTURE)
 	@Temporal(TemporalType.TIMESTAMP)
-	private Moment				startMoment;
+	private Date				startMoment;
 
 	@Mandatory
 	@ValidMoment(constraint = ValidMoment.Constraint.ENFORCE_FUTURE)
 	@Temporal(TemporalType.TIMESTAMP)
-	private Moment				endMoment;
+	private Date				endMoment;
 
 	@Optional
 	@ValidUrl
