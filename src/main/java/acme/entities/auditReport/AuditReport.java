a626729477dd8d3eded4dc5c7f4c99610512eb72
Ronald752 <ronmonalb@alum.us.es>
2026-02-16 01:50:09 +0100

    feat: Add entitis

---
+++ src/main/java/acme/entities/auditReport/AuditReport.java
@@ -0,0 +1,101 @@
+
+package acme.entities.auditReport;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoment.Constraint;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidHeader;
+import acme.constraints.ValidText;
+import acme.constraints.ValidTicker;
+import acme.features.auditor.auditReport.AuditReportRepository;
+import acme.realms.Auditor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AuditReport extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long		serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidTicker
+	@Column(unique = true)
+	private String					ticker;
+
+	@Mandatory
+	@ValidHeader
+	@Column
+	private String					name;
+
+	@Mandatory
+	@ValidText
+	@Column
+	private String					description;
+
+	@Mandatory
+	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date					startMoment;
+
+	@Mandatory
+	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date					endMoment;
+
+	@Optional
+	@ValidUrl
+	@Column
+	private String					moreInfo;
+
+	@Mandatory
+	@Valid
+	@Column
+	private Boolean					draftMode;
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Auditor					auditor;
+
+	// Derived attributes -----------------------------------------------------
+
+	@Transient
+	@Autowired
+	private AuditReportRepository	repository;
+
+	@Mandatory
+	@Valid
+	@Transient
+	private Double					monthsActive;
+
+	@Mandatory
+	@ValidNumber(min = 0)
+	@Transient
+	private Integer					hours;
+
+}
