62848ce7b7f8dab2a5bf59f8d3fe9cf900125f02
MaxCorti1 <maxcor@alum.us.es>
2026-02-21 10:37:16 +0100

    Fix: tickers, relations, effort and monthsActive fixed.

--- src/main/java/acme/entities/campaign/Milestone.java
+++ src/main/java/acme/entities/campaign/Milestone.java
@@ -14,16 +14,10 @@ package acme.entities.campaign;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
-import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.validation.Mandatory;
-import acme.client.components.validation.ValidNumber;
-import acme.constraints.ValidHeader;
-import acme.constraints.ValidText;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -39,23 +33,22 @@ public class Milestone extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@Mandatory
-	@ValidHeader
+	// @ValidHeader
 	@Column
 	private String				title;
 
 	@Mandatory
-	@ValidText
+	// @ValidText
 	@Column
 	private String				achievements;
 
 	@Mandatory
-	@ValidNumber(min = 0.0)
+	// @ValidNumber(min = 0.0)
 	@Column
 	private Double				effort;
 
 	@Mandatory
-	@Valid
-	@Enumerated(EnumType.STRING)
+	// @Valid
 	@Column
 	private MilestoneKind		kind;
 
@@ -64,7 +57,7 @@ public class Milestone extends AbstractEntity {
 	// Relationships ----------------------------------------------------------
 
 	@Mandatory
-	@Valid
+	// @Valid
 	@ManyToOne(optional = false)
 	private Campaign			campaign;
 }
