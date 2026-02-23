65478c33efe2234a8f57a08cd52243bf25d957fe
sbf6606 <fraayadia@alum.us.es>
2026-02-23 11:00:19 +0100

    fix: Validador de invention completado

--- src/main/java/acme/constraints/InventionValidator.java
+++ src/main/java/acme/constraints/InventionValidator.java
@@ -11,13 +11,17 @@ import acme.client.components.validation.AbstractValidator;
 import acme.client.components.validation.Validator;
 import acme.client.helpers.MomentHelper;
 import acme.entities.invention.Invention;
+import acme.features.inventor.invention.InventionRepository;
 import acme.features.inventor.part.PartRepository;
 
 @Validator
 public class InventionValidator extends AbstractValidator<ValidInvention, Invention> {
 
 	@Autowired
-	private PartRepository repository;
+	private PartRepository		partRepository;
+
+	@Autowired
+	private InventionRepository	inventionRepository;
 
 
 	@Override
@@ -32,19 +36,33 @@ public class InventionValidator extends AbstractValidator<ValidInvention, Invent
 
 		if (invention == null)
 			return true;
+		else {
+			{
+				Invention existingInvention = this.inventionRepository.findInventionByTicker(invention.getTicker());
+				boolean uniqueInvention = existingInvention != null && existingInvention.equals(invention);
 
-		Date now = MomentHelper.getBaseMoment();
-		Date start = invention.getStartMoment();
-		Date end = invention.getEndMoment();
+				super.state(context, uniqueInvention, "ticker", "acme.validation.invention.ticker.non-unique");
 
-		boolean validDates = start != null && end != null && !start.before(now) && end.after(start);
-		super.state(context, validDates, "*", "acme.validation.invention.dates.error");
+			}
+			{
+				if (invention.getDraftMode() != null && !invention.getDraftMode()) {
+					Integer partsCount = this.partRepository.countPartsByInventionId(invention.getId());
+					boolean hasParts = partsCount != null && partsCount > 0;
 
-		if (invention.getDraftMode() != null && !invention.getDraftMode()) {
-			Integer partsCount = this.repository.countPartsByInventionId(invention.getId());
-			boolean hasParts = partsCount != null && partsCount > 0;
+					super.state(context, hasParts, "draftMode", "acme.validation.invention.parts.error");
+				}
+			}
+			{
+				Date now = MomentHelper.getBaseMoment();
+				Date start = invention.getStartMoment();
+				Date end = invention.getEndMoment();
+				boolean datesInFuture = start != null && end != null && !start.before(now) && end.after(start);
 
-			super.state(context, hasParts, "draftMode", "acme.validation.invention.parts.error");
+				boolean validPublishedInvention = !invention.getDraftMode() && datesInFuture;
+
+				super.state(context, validPublishedInvention, "*", "acme.validation.invention.dates.error");
+			}
+
 		}
 
 		return !super.hasErrors(context);
